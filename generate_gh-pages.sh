788345b2b1f79684fe0e6d043b24017c3fdab8b6
mbohun <martin.bohun@gmail.com>
2019-06-06 23:05:55 +1000

    CLEANUP: reformatted the script, in default mode (when invoked without any args) the script just generates the HTML and exits; the user has to explicitely ask (-p) to commit and push the generated stuff to the remote gh-pages branch.

--- generate_gh-pages.sh
+++ generate_gh-pages.sh
@@ -7,47 +7,69 @@ set -e
 #       - as a build script (for eaxmple run on travis-ci.org, circleci.com, Jenkins, etc.)
 #
 PROGRAM_NAME="`basename ${0}`"
+
 TMP_WORK_DIR=`mktemp -d /tmp/${PROGRAM_NAME}.XXXXXX`
 echo $TMP_WORK_DIR
 
 BKCHEM_GIT_SHA=`git log -n1 --format=format:%H bkchem`
 
-mogrify -path $TMP_WORK_DIR -format png -background white bkchem/*.svg
-#ls -lahF $TMP_WORK_DIR
+function generate_gh-pages {
+    mogrify -path $TMP_WORK_DIR -format png -background white bkchem/*.svg
+    #ls -lahF $TMP_WORK_DIR
 
-# create thumbs
-mkdir $TMP_WORK_DIR/thumbs
-mogrify  -path $TMP_WORK_DIR/thumbs -format png  -thumbnail 25% $TMP_WORK_DIR/*.png
+    # create thumbs
+    mkdir $TMP_WORK_DIR/thumbs
+    mogrify  -path $TMP_WORK_DIR/thumbs -format png  -thumbnail 25% $TMP_WORK_DIR/*.png
 
-# create index.html
-INDEX_HTML=$TMP_WORK_DIR/index.html
-echo '<html>' > $INDEX_HTML
-echo '    <body style="background-color:#000000">' >> $INDEX_HTML
-echo '    <a href="https://github.com/mbohun/molecules" style="color:#ffffff">' >> $INDEX_HTML
-echo '        <h1 style="color:#ffffff">mbohun.github.io/molecules</h1>' >> $INDEX_HTML
-echo '    </a>' >> $INDEX_HTML
-echo '    <a href="https://github.com/mbohun/molecules/tree/${BKCHEM_GIT_SHA}/bkchem" style="color:#ffffff">' >> $INDEX_HTML
-echo '        <h3 style="color:#ffffff">${BKCHEM_GIT_SHA}</h3>' >> $INDEX_HTML
-echo '    </a>' >> $INDEX_HTML
-for img in `ls $TMP_WORK_DIR/*.png`
-do
-    b_img=`basename $img`
-    echo "    <a href=\"${b_img}\"><img src=\"thumbs/${b_img}\" title=\"${b_img%.*}\"/></a>" >> $INDEX_HTML
+    # create index.html
+    local index_html=$TMP_WORK_DIR/index.html
+
+    echo '<html>' > $index_html
+    echo '    <body style="background-color:#000000">' >> $index_html
+    echo '        <a href="https://github.com/mbohun/molecules" style="color:#ffffff; font-size:42px; text-decoration:none">mbohun.github.io/molecules</a>' >> $index_html
+    echo '        <p/>' >> $index_html
+    echo "        <a href=\"https://github.com/mbohun/molecules/tree/${BKCHEM_GIT_SHA}/bkchem\" style=\"color:#aaaaaa; text-decoration:none\">${BKCHEM_GIT_SHA}</a>" >> $index_html
+    echo '        <p/>' >> $index_html
+
+    for img in `ls $TMP_WORK_DIR/*.png`
+    do
+	local b_img=`basename $img`
+	echo "    <a href=\"${b_img}\"><img src=\"thumbs/${b_img}\" title=\"${b_img%.*}\"/></a>" >> $index_html
+    done
+
+    echo '    </body>' >> $index_html
+    echo '</html>' >> $index_html
+}
+
+function push_gh-pages {
+    git checkout gh-pages
+
+    git rm -r thumbs *.png index.html
+    cp -r $TMP_WORK_DIR/* .
+    git add .
+
+    git commit -m"AUTO: regenerated branch gh-pages from master (${BKCHEM_GIT_SHA})."
+    git push --set-upstream origin gh-pages
+
+    git checkout master
+}
+
+while getopts ":p" opt; do
+    case $opt in
+	p)
+	    generate_gh-pages
+	    push_gh-pages
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    exit 1
+	    ;;
+    esac
 done
 
-echo '    </body>' >> $INDEX_HTML
-echo '</html>' >> $INDEX_HTML
-
-git checkout gh-pages
-
-git rm -r thumbs *.png index.html
-cp -r $TMP_WORK_DIR/* .
-git add .
-
-git commit -m"AUTO: regenerated branch gh-pages from master (${BKCHEM_GIT_SHA})."
-git push --set-upstream origin gh-pages
-
-git checkout master
+# NOTE: unless the user asked to push gh-pages (-p) we just generate HTML and exit
+generate_gh-pages
 
 # cleanup
 #rm -rf $TMP_WORK_DIR
